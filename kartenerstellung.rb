e47d5bec6175ad385ab553ed7a2b4f44078a88b0
Udo Wiegärtner <kosmonaut@gmx.net>
2026-01-08 22:20:46 +0100

    Bildplazierung funktioniert jetzt. Ca. 36 Charaktere schon enthalten.

--- kartenerstellung.rb
+++ kartenerstellung.rb
@@ -1,5 +1,6 @@
 require 'squib'
 require 'roo'
+require 'mini_magick'
 
 xlsx = Roo::Excelx.new('ClashRoyaleKartendaten.xlsx')
 daten = {
@@ -23,11 +24,33 @@ Squib::Deck.new cards: daten['name'].size, layout: 'layout.yml' do
   text str: daten['name'], layout: 'title'
   text str: daten['nummer'], layout: 'lower_right'
   
-  png_files = daten['url_pfadname'].map do |b|
+  # Hole Layout-Werte
+  art_y = layout['art']['y']
+  art_h = layout['art']['height']
+  area_x = layout['art_area']['x']
+  area_w = layout['art_area']['width']
+  center_x = area_x + (area_w / 2.0)
+  
+  # Rendere jedes Bild einzeln zentriert
+  daten['url_pfadname'].each_with_index do |b, i|
     pfad = "bilder/#{b}.png"
-    File.exist?(pfad) ? pfad : nil
+    if File.exist?(pfad)
+      # Lade Bild mit MiniMagick um Dimensionen zu ermitteln
+      img = MiniMagick::Image.open(pfad)
+      original_width = img.width
+      original_height = img.height
+      
+      # Berechne skalierte Breite bei Höhe 400
+      scale_factor = art_h.to_f / original_height
+      scaled_width = (original_width * scale_factor).round
+      
+      # Berechne x so, dass Bild zentriert ist (linke Kante)
+      x_pos = center_x - (scaled_width / 2.0)
+      
+      # Rendere Bild
+      png range: i, file: pfad, x: x_pos, y: art_y, height: art_h
+    end
   end
-  png file: png_files, layout: 'art', scale: 'fit'
   
   character_werte = (0...daten['name'].size).map do |i|
     "Elixier: #{daten['elexier'][i]}\n" \
